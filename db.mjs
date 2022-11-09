04dfd891d9a2bf8db9c3dbd13cd56bae68ec300a
Yangge Zheng <yanggezheng@10-16-6-19.dynapool.wireless.nyu.edu>
2022-11-08 22:46:42 -0500

    haha

--- db.mjs
+++ db.mjs
@@ -1,23 +1,4 @@
 // is the environment variable, NODE_ENV, set to PRODUCTION? 
-import fs from 'fs';
-import path from 'path';
-import url from 'url';
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-let dbconf;
-if (process.env.NODE_ENV === 'PRODUCTION') {
- // if we're in PRODUCTION mode, then read the configration from a file
- // use blocking file io to do this...
- const fn = path.join(__dirname, 'config.json');
- const data = fs.readFileSync(fn);
-
- // our configuration file will be in json, so parse it and set the
- // conenction string appropriately!
- const conf = JSON.parse(data);
- dbconf = conf.dbconf;
-} else {
- // if we're not in PRODUCTION mode, then use
- dbconf = 'mongodb://localhost/hw05';
-}
 // 1ST DRAFT DATA MODEL
 // users
 // * our site requires authentication...
@@ -47,13 +28,31 @@ ArticleSchema.plugin(mongooseSlugPlugin, {tmpl: '<%=title%>'});
 mongoose.model('User', UserSchema);
 mongoose.model('Article', ArticleSchema);
 // mongoose.connect('mongodb://localhost/hw05');
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ const fn = path.join(__dirname, 'config.json');
+ const data = fs.readFileSync(fn);
 
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/hw05';
+}
 console.log("____________________________________");
 console.log("--->", dbconf, "<---");
 console.log("____________________________________");
 
 
-await mongoose.connect(dbconf);
+mongoose.connect(dbconf);
 
 //   General information in a to do list
 // * includes some basic information about the task (the due date, penalty, etc)
