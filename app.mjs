28b208c5c92d105a06c983206220cbc17230aa70
Yangge Zheng <yanggezheng@10-16-6-19.dynapool.wireless.nyu.edu>
2022-11-08 21:24:13 -0500

    second milestone

--- app.mjs
+++ app.mjs
@@ -1,10 +1,151 @@
-import express from 'express'
-import path from 'path'
-import * as myModule from '../final-project-yanggezheng/db';
-import { fileURLToPath } from 'url';
+import './db.mjs';
+import express from 'express';
+import mongoose from 'mongoose';
+import session from 'express-session';
+
+import path from 'path';
+import url from 'url';
+import * as auth from './auth.mjs';
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+const Article = mongoose.model('Article');
+
+const loginMessages = {"PASSWORDS DO NOT MATCH": 'Incorrect password', "USER NOT FOUND": 'User doesn\'t exist'};
+const registrationMessages = {"USERNAME ALREADY EXISTS": "Username already exists", "USERNAME PASSWORD TOO SHORT": "Username or password is too short"};
+
+
+///////////////////////
+// CUSTOM MIDDLEWARE //
+///////////////////////
+
+// require authenticated user for /article/add path
+app.use(auth.authRequired(['/article/add']));
+
+// make {{user}} variable available for all paths
+app.use((req, res, next) => {
+  res.locals.user = req.session.user;
+  next();
+});
+
+// logging
+app.use((req, res, next) => {
+  console.log(req.method, req.path, req.body);
+  next();
+});
+
+////////////////////
+// ROUTE HANDLERS //
+////////////////////
+app.get('/', (req, res) => {
+  Article.find({}).sort('-createdAt').exec((err, articles) => {
+    res.render('index', {user: req.session.user, home: true, articles: articles});
+  });
+});
+
+app.get('/article/add', (req, res) => {
+  if (!req.session.user){
+    res.redirect("/login");
+  }else{
+    res.render('article-add');
+  }
+
+});
+
+app.post('/article/add', (req, res) => {
+  //complete POST /article/add
+  if (!req.session.user){
+    res.redirect("/login");
+  }else{
+    const newArticle = new Article({
+      title: req.body.title,
+      url: req.body.url,
+      description: req.body.description,
+      user: req.session.user._id
+    });
+    newArticle.save(err => {
+      if (err){
+        console.log(err);
+        res.render('article-add', {error: err});
+      }else{
+        res.redirect("/");
+      }
+    });
+  }
+});
+
+app.get('/article/:slug', (req, res) => {
+  //complete GET /article/slug
+  Article.findOne({slug: req.params.slug})
+  .populate("user")
+  .exec((err, result) => {
+    if (err){
+      res.render('error', {message: "error"});
+    }else{
+      res.render('article-detail', {article: result});
+    }
+  });
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  // setup callbacks for register success and error
+  function success(newUser) {
+    auth.startAuthenticatedSession(req, newUser, (err) => {
+        if (!err) {
+            res.redirect('/');
+        } else {
+            res.render('error', {message: 'err authing???'}); 
+        }
+    });
+  }
+
+  function error(err) {
+    res.render('register', {message: registrationMessages[err.message] ?? 'Registration error'}); 
+  }
+
+  // attempt to register new user
+  auth.register(req.body.username, req.body.email, req.body.password, error, success);
+});
+        
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  // setup callbacks for login success and error
+  function success(user) {
+    auth.startAuthenticatedSession(req, user, (err) => {
+      if(!err) {
+        res.redirect('/'); 
+      } else {
+        res.render('error', {message: 'error starting auth sess: ' + err}); 
+      }
+    }); 
+  }
+
+  function error(err) {
+    res.render('login', {message: loginMessages[err.message] || 'Login unsuccessful'}); 
+  }
+
+  // attempt to login
+  auth.login(req.body.username, req.body.password, error, success);
+});
 
 app.listen(process.env.PORT || 3000);
---
+++ db.mjs
@@ -0,0 +1,80 @@
+// // is the environment variable, NODE_ENV, set to PRODUCTION? 
+// import fs from 'fs';
+// import path from 'path';
+// import url from 'url';
+// const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+// let dbconf;
+// if (process.env.NODE_ENV === 'PRODUCTION') {
+//  // if we're in PRODUCTION mode, then read the configration from a file
+//  // use blocking file io to do this...
+//  const fn = path.join(__dirname, 'config.json');
+//  const data = fs.readFileSync(fn);
+
+//  // our configuration file will be in json, so parse it and set the
+//  // conenction string appropriately!
+//  const conf = JSON.parse(data);
+//  dbconf = conf.dbconf;
+// } else {
+//  // if we're not in PRODUCTION mode, then use
+//  dbconf = 'mongodb://localhost/YOUR_DATABASE_NAME_HERE';
+// }
+// 1ST DRAFT DATA MODEL
+// users
+// * our site requires authentication...
+// * so users have a username and password
+// * in order to make the account secure, the passward will be hashed
+
+  // username provided by authentication plugin
+  // password hash provided by authentication plugin
+import mongooseSlugPlugin from 'mongoose-slug-plugin';
+import mongoose from 'mongoose';
+const UserSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, unique: true, required: true}
+  });
+
+  const ArticleSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    url: {type: String, required: true},
+    description: {type: String, required: true},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+  });
+
+// configure plugin
+ArticleSchema.plugin(mongooseSlugPlugin, {tmpl: '<%=title%>'});
+// register models
+mongoose.model('User', UserSchema);
+mongoose.model('Article', ArticleSchema);
+mongoose.connect('mongodb://localhost/hw05');
+// mongoose.connect(dbconf);
+
+//   General information in a to do list
+// * includes some basic information about the task (the due date, penalty, etc)
+// * a list can have 0 to 5 tasksff
+// const List = new mongoose.Schema({
+//   name: {type: String, required: true},
+//   dueDate: {type: Number, min: 1000, required: true},
+//   teamWork: {type: Boolean, default: false, required: true},
+//   important: {type: Boolean, default: true, required: true},
+//   penalty: {type: String, required: false},
+//   teamMates: {type: Boolean, default: false, required: false},
+//   detail: [detail]
+// }, {
+//   _id: true
+// });
+
+// a task can be break down into small pieces
+// * each list must have a related general task
+// const detail = new mongoose.Schema({
+//     name: "AIT HW5",
+//     detailedTasks: [
+//       { name: "part1", status: "did not start",  teamWork: false, teamMates : {}},
+//       { name: "part2", status: "did not start",  teamWork: false, teamMates : {}},
+//       { name: "part3", status: "did not start",  teamWork: false, teamMates : {}}
+//     ],
+//     createdAt: 1025// current time
+
+// TODO: add remainder of setup for slugs, connection, registering models, etc. below
+
+// });
