c460d7d409b7f3be999f436d95166e48e5c8ac1c
Yangge Zheng <yanggezheng@10-16-6-19.dynapool.wireless.nyu.edu>
2022-11-08 23:06:15 -0500

    haha

--- db.mjs
+++ db.mjs
@@ -33,11 +33,11 @@ import path from 'path';
 import url from 'url';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 let dbconf;
-console.log(process.env.NODE_ENV,"dsadsfsadasdsasdfsdfsdfsdfsdf");
 if (process.env.NODE_ENV === 'PRODUCTION') {
  // if we're in PRODUCTION mode, then read the configration from a file
  // use blocking file io to do this...
  const fn = path.join(__dirname, 'config.json');
+ console.log(fn);
  const data = fs.readFileSync(fn);
 
  // our configuration file will be in json, so parse it and set the
@@ -48,9 +48,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
  // if we're not in PRODUCTION mode, then use
  dbconf = 'mongodb://localhost/hw05';
 }
-console.log("____________________________________");
-console.log("--->", dbconf, "<---");
-console.log("____________________________________");
+
 
 
 mongoose.connect(dbconf);
