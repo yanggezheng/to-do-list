a2d0a0d5e880a1eb81a0050c6269f2e3b0ab48fd
Yangge Zheng <yanggezheng@10-20-193-97.dynapool.wireless.nyu.edu>
2022-12-03 19:09:37 -0500

    deploy

--- src/db.mjs
+++ src/db.mjs
@@ -28,6 +28,10 @@ TaskSchema.plugin(mongooseSlugPlugin, {tmpl: '<%=title%>'});
 mongoose.model('User', UserSchema);
 mongoose.model('Task', TaskSchema);
 mongoose.model('Friend', FriendSchema);
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 let dbconf;
   if (process.env.NODE_ENV === 'PRODUCTION') {
    // if we're in PRODUCTION mode, then read the configration from a file
